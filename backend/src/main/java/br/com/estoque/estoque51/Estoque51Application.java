a30b9253679173b25e8e948cc323004a250cc0f4
Carlos Monteiro <carlosmonteiro1985@outlook.com>
2021-09-06 23:00:26 -0300

    Criação do projeto

---
+++ backend/src/main/java/br/com/estoque/estoque51/Estoque51Application.java
@@ -0,0 +1,13 @@
+package br.com.estoque.estoque51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Estoque51Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Estoque51Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/estoque/estoque51/Estoque51ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estoque.estoque51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Estoque51ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
